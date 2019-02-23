bd07d31d3d7309d82f9faf46eb2087c048c695eb
Oleg Zaidullin <cornknight@gmail.com>
2019-02-23 14:34:17 +0600

    main parameters became const val

--- src/main/kotlin/com/corn/tetris/Tetris.kt
+++ src/main/kotlin/com/corn/tetris/Tetris.kt
@@ -1,11 +1,12 @@
 package com.corn.tetris
 
+const val CELL_SIZE = 50.0
+const val GAP = 3.0
+const val COLS = 10
+const val ROWS = 18
+
 class Tetris {
 
-    private val CELL_SIZE = 50.0
-    private val GAP = 3.0
-    private val COLS = 10
-    private val ROWS = 18
+    val container: TContainer = TContainer(COLS, ROWS, GAP, CELL_SIZE)
 
-    val container: TContainer = TContainer(COLS,ROWS,GAP,CELL_SIZE)
 }
